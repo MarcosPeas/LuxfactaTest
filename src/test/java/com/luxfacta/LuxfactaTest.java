46cdd38dfbd9294c7e0c4796f8592566f07f98ce
Marcos Antonio <peas.shadow@gmail.com>
2021-04-15 21:34:23 -0300

    Finaliza os testes da classe Luxfacta

--- src/test/java/com/luxfacta/LuxfactaTest.java
+++ src/test/java/com/luxfacta/LuxfactaTest.java
@@ -6,8 +6,26 @@ import org.junit.Test;
 public class LuxfactaTest {
 
     @Test
+    public void numberDivisibleByThreeTest() {
+        Luxfacta luxfacta = new Luxfacta(9);
+        Assert.assertEquals("Lux", luxfacta.say());
+    }
+
+    @Test
+    public void numberDivisibleByFiveTest() {
+        Luxfacta luxfacta = new Luxfacta(10);
+        Assert.assertEquals("Facta", luxfacta.say());
+    }
+
+    @Test
     public void numberDivisibleByThreeAndFiveTest() {
         Luxfacta luxfacta = new Luxfacta(15);
         Assert.assertEquals("LuxFacta", luxfacta.say());
+    }
+
+    @Test
+    public void numberNotDivisibleByThreeOrFive() {
+        Luxfacta luxfacta = new Luxfacta(13);
+        Assert.assertEquals("13", luxfacta.say());
     }
 }
