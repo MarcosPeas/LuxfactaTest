6d097bf1f01099f59385bfedd1ece779d98d9ce1
Marcos Antonio <peas.shadow@gmail.com>
2021-04-15 21:24:17 -0300

    Finaliza a classe Path

--- src/main/java/com/luxfacta/Path.java
+++ src/main/java/com/luxfacta/Path.java
@@ -13,8 +13,8 @@ public class Path {
     }
 
     Path(String root) {
-        boolean matches = root.matches("(/[a-zA-Z]+([/]?))*");
-        if (!matches) {
+        boolean matches = rootMatches(root);
+        if (!matches || root.isBlank()) {
             throw new InvalidPathException(root, "o caminho está incorreto. Utilize o padrão /a/b ou /a/b/");
         }
         root = root.substring(1);
@@ -25,4 +25,54 @@ public class Path {
         });
     }
 
+    public boolean rootMatches(String dir) {
+        return dir.matches("(/[a-zA-Z]+([/]?))*");
+    }
+
+    public String cd(String dir) {
+        if (addDirectoryMatches(dir)) {
+            return addDirectory(dir);
+        } else if (toFatherMatches(dir)) {
+            return returnToFather(dir);
+        }
+        throw new InvalidPathException(dir, "o caminho está incorreto");
+    }
+
+    public boolean toFatherMatches(String dir) {
+        return dir.matches("([\\.]{2}/)+([a-zA-Z]*[/]?)*");
+    }
+
+    public boolean addDirectoryMatches(String dir) {
+        return dir.matches("[/]?([a-zA-Z]+[/]?)+");
+    }
+
+
+    String addDirectory(String dir) {
+        String[] split = dir.split("/");
+        Arrays.stream(split).forEach(s -> {
+            if (s.isBlank()) return;
+            stack.add("/");
+            stack.add(s);
+        });
+        return toString();
+    }
+
+    String returnToFather(String dir) {
+        while (dir.contains("../")) {
+            dir = dir.replaceFirst("[\\.]{2}/", "");
+            stack.pop();
+            stack.pop();
+        }
+        String[] split = dir.split("/");
+        Arrays.stream(split).forEach(s -> {
+            if (s.isBlank()) return;
+            stack.add("/");
+            stack.add(s);
+        });
+        return toString();
+    }
+
+    public String toString() {
+        return stack.stream().reduce("", (s, s2) -> s + s2, (s, s2) -> s + s2);
+    }
 }
--- src/test/java/com/luxfacta/PathTest.java
+++ src/test/java/com/luxfacta/PathTest.java
@@ -10,24 +10,29 @@ public class PathTest {
     @Test
     public void rootWithBarOnStartTest() {
         Path path = new Path();
-        Assert.assertTrue(path.matches("/a/b/c"));
+        Assert.assertTrue(path.rootMatches("/a/b/c"));
     }
 
     @Test
     public void rootWithNotBarOnStartTest() {
         Path path = new Path();
-        Assert.assertTrue(paht.matches("a/b/c"));
+        Assert.assertFalse(path.rootMatches("a/b/c"));
     }
 
     @Test
     public void rootWithBarOnEndTest() {
         Path path = new Path();
-        Assert.assertTrue(paht.matches("a/b/c/"));
+        Assert.assertTrue(path.rootMatches("/a/b/c/"));
     }
 
-    @Test(expected = InvalidPathException.class)
+    @Test
     public void rootWithEspecialCharacteresPathTest() {
-        new Path("/a/?");
+        try {
+            new Path("/a/?");
+            Assert.fail("Espera-se uma InvalidPathException");
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof InvalidPathException);
+        }
     }
 
     @Test(expected = InvalidPathException.class)
@@ -37,13 +42,13 @@ public class PathTest {
 
     @Test
     public void toAddDirectoryTest() {
-        Path path = new Path("a/b");
+        Path path = new Path("/a/b");
         Assert.assertTrue(path.addDirectoryMatches("/c/d"));
     }
 
     @Test
     public void toFatherDirectoryTest() {
-        Path path = new Path("a/b/");
+        Path path = new Path("/a/b/");
         Assert.assertTrue(path.toFatherMatches("../"));
         Assert.assertTrue(path.toFatherMatches("../../"));
         Assert.assertTrue(path.toFatherMatches("../../x/y"));
@@ -51,23 +56,28 @@ public class PathTest {
 
     @Test
     public void addPathTest() {
-        Path path = new Path("a/b/c");
+        Path path = new Path("/a/b/c");
         String dir = path.cd("e/f");
         Assert.assertEquals("/a/b/c/e/f", dir);
     }
 
     @Test
     public void gotoFatherPathTest() {
-        Path path = new Path("a/b/c/d/e/f");
+        Path path = new Path("/a/b/c/d/e/f");
         String dir = path.cd("../m");
         Assert.assertEquals("/a/b/c/d/e/m", dir);
         dir = path.cd("../../n");
         Assert.assertEquals("/a/b/c/d/n", dir);
     }
 
-    @Test(expected = InvalidPathException.class)
+    @Test
     public void changeDirectoryWithInvalidCharactereTest() {
-        Path path = new Path("a/b/c/d/e/f");
-        path.cd("/o/?");
+        try {
+            Path path = new Path("/a/b/c/d/e/f");
+            path.cd("/o/?");
+            Assert.fail("Espera-se uma InvalidPathException");
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof InvalidPathException);
+        }
     }
 }
